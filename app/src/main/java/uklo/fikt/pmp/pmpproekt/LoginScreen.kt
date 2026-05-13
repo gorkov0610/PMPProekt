da4d1256ee798bdc16d1af7a02b10c46a467ed5d
gorkov0610 <gorkov.hristijan@uklo.edu.mk>
2026-05-13 15:49:08 +0200

    Changed the login screen to implement login with facebook

--- app/src/main/java/uklo/fikt/pmp/pmpproekt/LoginScreen.kt
+++ app/src/main/java/uklo/fikt/pmp/pmpproekt/LoginScreen.kt
@@ -1,27 +1,37 @@
 package uklo.fikt.pmp.pmpproekt
 
+import android.app.Activity
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,10 +42,21 @@ import uklo.fikt.pmp.pmpproekt.ui.theme.EmeraldPrimary
 import uklo.fikt.pmp.pmpproekt.ui.theme.SlateSecondary
 import androidx.compose.ui.res.stringResource
 
+import com.facebook.login.LoginManager
 @Composable
 fun LoginScreen(onLoginSuccess : () -> Unit){
     val context = LocalContext.current
     val authManager = remember { AuthManager(context) }
+    DisposableEffect(Unit) {
+        authManager.handleFacebookLogin { success, errorMessage ->
+            if (success) {
+                onLoginSuccess()
+            } else {
+                Log.e("FB_Auth", "Грешка: $errorMessage")
+            }
+        }
+        onDispose {}
+    }
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -43,7 +64,6 @@ fun LoginScreen(onLoginSuccess : () -> Unit){
         try {
             val account = task.getResult(ApiException::class.java)
             account?.idToken?.let { token ->
-                // Ова е клучниот дел:
                 authManager.signInWithGoogle(token) { success ->
                     if (success) {
                         onLoginSuccess()
@@ -94,27 +114,56 @@ fun LoginScreen(onLoginSuccess : () -> Unit){
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(56.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = EmeraldPrimary
-                    ),
+                    border = BorderStroke(1.dp, Color.LightGray),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black),
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Image(
+                            painter = painterResource(R.drawable.google_logo),
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Text(
+                            text = stringResource(R.string.signin_google),
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Medium,
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        LoginManager.getInstance().logInWithReadPermissions(
+                            context as Activity,
+                            listOf("email", "public_profile")
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1877F2)),
                     shape = RoundedCornerShape(16.dp),
                     elevation = ButtonDefaults.buttonElevation(4.dp)
                 ) {
-                    Text(
-                        text = stringResource(R.string.signin_google),
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Medium,
-                        color = Color.White
-                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Icon(
+                            painter = painterResource(id = R.drawable.facebook_logo),
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(Modifier.width(12.dp))
+                        Text(stringResource(R.string.signin_fb), color = Color.White)
+                    }
                 }
-
-                Spacer(modifier = Modifier.height(24.dp))
-
-                Text(
-                    text = stringResource(R.string.signin_google_text),
-                    style = MaterialTheme.typography.labelSmall,
-                    color = Color.LightGray
-                )
             }
         }
     }
--- app/src/main/java/uklo/fikt/pmp/pmpproekt/data/AuthManager.kt
+++ app/src/main/java/uklo/fikt/pmp/pmpproekt/data/AuthManager.kt
@@ -2,17 +2,52 @@ package uklo.fikt.pmp.pmpproekt.data
 
 import android.content.Context
 import android.content.Intent
+import com.facebook.CallbackManager
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import uklo.fikt.pmp.pmpproekt.R
 
 class AuthManager (private val context: Context){
     private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+    private val callbackManager = CallbackManager.Factory.create()
 
+    fun getCallbackManager() = callbackManager
+
+    fun handleFacebookLogin(onResult: (Boolean, String?) -> Unit) {
+        LoginManager.getInstance().registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                    auth.signInWithCredential(credential)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                onResult(true, null)
+                            } else {
+                                onResult(false, task.exception?.message)
+                            }
+                        }
+                }
+
+                override fun onCancel() {
+                    onResult(false, "Canceled")
+                }
+
+                override fun onError(error: FacebookException) {
+                    onResult(false, error.message)
+                }
+            }
+        )
+    }
     fun getGoogleSignInClient() : GoogleSignInClient{
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(context.getString(R.string.default_web_client_id))
@@ -32,3 +67,4 @@ class AuthManager (private val context: Context){
     fun getCurrentUser() = auth.currentUser
     fun signOut() = auth.signOut()
 }
+
