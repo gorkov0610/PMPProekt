00556f2414e149d75c17fc112c8a955709b84b29
gorkov0610 <gorkov.hristijan@uklo.edu.mk>
2026-06-02 00:00:24 +0200

    Removed the local messages, all messaging and notifications now use FCM

--- app/src/main/java/uklo/fikt/pmp/pmpproekt/data/LikeManager.kt
+++ app/src/main/java/uklo/fikt/pmp/pmpproekt/data/LikeManager.kt
@@ -4,8 +4,7 @@ import android.content.Context
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import uklo.fikt.pmp.pmpproekt.PreferenceManager
-import uklo.fikt.pmp.pmpproekt.R
-import uklo.fikt.pmp.pmpproekt.showLocalNotification
+
 
 fun toggleLikeSkill(skill: Skill, currentUserId: String, db: FirebaseFirestore, context: Context) {
     val appContext = context.applicationContext
@@ -23,18 +22,6 @@ fun toggleLikeSkill(skill: Skill, currentUserId: String, db: FirebaseFirestore, 
         skillRef.update(
             "likesCount", FieldValue.increment(1),
             "likedBy", FieldValue.arrayUnion(currentUserId)
-        ).addOnSuccessListener {
-            val nextLikesCount = skill.likesCount + 1
-            if (nextLikesCount == 1 || nextLikesCount == 2 || nextLikesCount == 100 || nextLikesCount == 500) {
-
-                showLocalNotification(
-                    context = appContext,
-                    title = appContext.getString(R.string.notification_like_title),
-                    message = appContext.getString(R.string.notification_like_message, skill.title, nextLikesCount),
-                    senderId = "",
-                    senderName = ""
-                )
-            }
-        }
+        )
     }
 }
